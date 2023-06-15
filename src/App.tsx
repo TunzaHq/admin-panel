0d1e9167aca05468f82d71b8bc8fdd659c132f73
Michael <mickeymurage@gmail.com>
2023-06-15 15:42:13 +0300

    added some packages to assist with auth added some more svgs styled the login, register and nav components added client-side routing to src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Loader from './App/Components/Loader/Loader';
-import Auth from './App/Components/Auth';
+import Login from './App/Components/Login';
 import Dashboard from './App/Components/Dashboard';
 import Navbar from './App/Components/Navbar';
 import Policies from './App/Components/Policies';
 import UserManagement from './App/Components/User';
+import Register from './App/Components/Register';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -24,15 +26,23 @@ function App() {
   if (isLoading) {
     return <Loader />;
   }
+  if (!isLoggedIn) window.location.href = '/login';
 
   return (
-    <div className="App">
-      {isLoggedIn && <Navbar />}
-      <div className="left-margin">
-        {isLoggedIn ? <Dashboard /> : <Auth />}
-        {isLoggedIn && <Policies />}
-        {isLoggedIn && <UserManagement />}
-      </div>
+    <div>
+        <BrowserRouter>
+            <Navbar>
+                <Routes>
+                    <Route path='/' element={<Login/>}/>
+                    <Route path='/dashboard' element={<Dashboard/>}/>
+                    <Route path='/register' element={<Register/>}/>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path='/policies' element={<Policies />} />
+                    <Route path='/new-policy' element={<Policies />} />
+                    <Route path='/user-management' element={<UserManagement />}/>
+                </Routes>
+            </Navbar>
+        </BrowserRouter>
     </div>
   );
 }
