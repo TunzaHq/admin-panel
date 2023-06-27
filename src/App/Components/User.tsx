a253b1964fbdefe35c05a2b2f139f148b19f229b
Paul-Ngigi <paulkush7777@gmail.com>
2023-06-27 12:10:13 +0300

    add authentication;

--- src/App/Components/User.tsx
+++ src/App/Components/User.tsx
@@ -1,26 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   makeGetRequest,
   makePutRequest,
   makeDeleteRequest,
-} from '../Api/ApiHandler';
-
-interface User {
-  id: number;
-  full_name: string;
-  avatar: string;
-  email: string;
-  location: string;
-  password: string;
-  role: string;
-  created_at: string;
-}
+} from "../Api/ApiHandler";
+import { IUser } from "../interfaces/user.interface";
 
 const UserManagement: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [selectedUser, setSelectedUser] = useState<User | null>(null);
-  const [message, setMessage] = useState<string>('');
-  const [error, setError] = useState<string>('');
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [selectedUser, setSelectedUser] = useState<IUser | null>(null);
+  const [message, setMessage] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
   useEffect(() => {
     fetchUsers();
@@ -28,43 +18,48 @@ const UserManagement: React.FC = () => {
 
   const fetchUsers = async () => {
     try {
-      const response = await makeGetRequest('/users');
+      const response = await makeGetRequest("/users");
       setUsers(response);
     } catch (error) {
-      console.error('Failed to fetch users:', error);
-      setError('Failed to fetch users. Please try again later.');
+      console.error("Failed to fetch users:", error);
+      setError("Failed to fetch users. Please try again later.");
     }
   };
 
-  const handleEditUser = (user: User) => {
+  const handleEditUser = (user: IUser) => {
     setSelectedUser(user);
-    setMessage('');
-    setError('');
+    setMessage("");
+    setError("");
   };
 
   const handleDeleteUser = async (userId: number) => {
     try {
       await makeDeleteRequest(`/users/${userId}`);
-      setUsers(users.filter(user => user.id !== userId));
-      setMessage('User deleted successfully.');
-      setError('');
+      setUsers(users.filter((user) => user.id !== userId));
+      setMessage("User deleted successfully.");
+      setError("");
     } catch (error) {
-      console.error('Failed to delete user:', error);
-      setError('Failed to delete user. Please try again later.');
+      console.error("Failed to delete user:", error);
+      setError("Failed to delete user. Please try again later.");
     }
   };
 
-  const handleUpdateUser = async (updatedUser: User) => {
+  const handleUpdateUser = async (updatedUser: IUser) => {
     try {
-      const response = await makePutRequest(`/users/${updatedUser.id}`, updatedUser);
-      const updatedUsers = users.map(user => (user.id === updatedUser.id ? response : user));
+      const response = await makePutRequest(
+        `/users/${updatedUser.id}`,
+        updatedUser
+      );
+      const updatedUsers = users.map((user) =>
+        user.id === updatedUser.id ? response : user
+      );
       setUsers(updatedUsers);
       setSelectedUser(null);
-      setMessage('User updated successfully.');
-      setError('');
+      setMessage("User updated successfully.");
+      setError("");
     } catch (error) {
-      console.error('Failed to update user:', error);
-      setError('Failed to update user. Please try again later.');
+      console.error("Failed to update user:", error);
+      setError("Failed to update user. Please try again later.");
     }
   };
 
@@ -87,15 +82,25 @@ const UserManagement: React.FC = () => {
         </thead>
         <tbody>
           {Array.isArray(users) ? (
-            users.map(user => (
+            users.map((user) => (
               <tr key={user.id}>
                 <td>{user.id}</td>
                 <td>{user.full_name}</td>
                 <td>{user.email}</td>
                 <td>{user.location}</td>
                 <td>
-                  <button className="btn btn-primary" onClick={() => handleEditUser(user)}>Edit</button>
-                  <button className="btn btn-danger" onClick={() => handleDeleteUser(user.id)}>Delete</button>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => handleEditUser(user)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDeleteUser(user.id)}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))
@@ -112,13 +117,14 @@ const UserManagement: React.FC = () => {
           <h2>Edit User</h2>
           <form onSubmit={() => handleUpdateUser(selectedUser)}>
             {/* Render input fields to update user information */}
-            <button type="submit" className="btn btn-primary">Save</button>
+            <button type="submit" className="btn btn-primary">
+              Save
+            </button>
           </form>
         </div>
       )}
     </div>
   );
 };
-
 
 export default UserManagement;
